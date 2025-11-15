3c3b8c4fb6f46609c0a517469ac04d12e0348c09
mdwasim1340 <60678774+mdwasim1340@users.noreply.github.com>
2025-11-15 11:57:41 +0530

    feat(bed): Add BedAssignmentController skeleton for Bed Management
    - REST handlers with validation, error propagation
    - CRUD, discharge, patient/bed history
    - Team: Beta, Phase: 4 - Controllers

---
+++ backend/src/controllers/bed-assignment.controller.ts
@@ -0,0 +1,89 @@
+import { Request, Response, NextFunction } from 'express';
+import { BedAssignmentService } from '../services/bed-assignment.service';
+import { CreateBedAssignmentSchema, UpdateBedAssignmentSchema, DischargeBedAssignmentSchema } from '../validation/bed.validation';
+
+export class BedAssignmentController {
+  private readonly service = new BedAssignmentService();
+
+  async listAssignments(req: Request, res: Response, next: NextFunction) {
+    try {
+      const tenantId = req.headers['x-tenant-id'] as string;
+      // TODO: support query params (bedId, patientId, pagination, status)
+      // const assignments = await this.service.
+      res.json({ /* assignments */ });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async createAssignment(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = CreateBedAssignmentSchema.parse(req.body);
+      const tenantId = req.headers['x-tenant-id'] as string;
+      const userId = req.user?.id;
+      // TODO: call service.createBedAssignment(data, tenantId, userId)
+      res.status(201).json({ /* assignment */ });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getAssignmentById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const assignmentId = Number(req.params.id);
+      const tenantId = req.headers['x-tenant-id'] as string;
+      // TODO: call service.getBedAssignmentById(assignmentId, tenantId)
+      res.json({ /* assignment */ });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateAssignment(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = UpdateBedAssignmentSchema.parse(req.body);
+      const assignmentId = Number(req.params.id);
+      const tenantId = req.headers['x-tenant-id'] as string;
+      const userId = req.user?.id;
+      // TODO: call service.updateBedAssignment(assignmentId, data, tenantId, userId)
+      res.json({ /* assignment */ });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async dischargeAssignment(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = DischargeBedAssignmentSchema.parse(req.body);
+      const assignmentId = Number(req.params.id);
+      const tenantId = req.headers['x-tenant-id'] as string;
+      const userId = req.user?.id;
+      // TODO: call service.dischargeBedAssignment(assignmentId, data, tenantId, userId)
+      res.json({ /* assignment */ });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getPatientHistory(req: Request, res: Response, next: NextFunction) {
+    try {
+      const patientId = Number(req.params.patientId);
+      const tenantId = req.headers['x-tenant-id'] as string;
+      // TODO: call service.getPatientBedHistory(patientId, tenantId)
+      res.json({ /* history */ });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getBedHistory(req: Request, res: Response, next: NextFunction) {
+    try {
+      const bedId = Number(req.params.bedId);
+      const tenantId = req.headers['x-tenant-id'] as string;
+      // TODO: call service.getBedAssignmentHistory(bedId, tenantId)
+      res.json({ /* history */ });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
